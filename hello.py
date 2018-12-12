6f1e8626f1d6cfe18f2cab5d15ca028af4cd7581
chewdonkey <chewdonkey@gmail.com>
2018-12-12 19:41:08 +0000

    added C++ hello world and shebang & comments to hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
+#!/usr/local/bin/python3
+#shebang for mac osx
 #!/usr/bin/env python3
+#shebang for linux
+
+#PROGRAM TITLE: hello
+#PROGRAM DESCRIPTION: prints hello world & acts as python3 template
+
+#import std lib
+#import 3rd party lib
+#import usr lib
+
+#global var
 
 print("hello world");
