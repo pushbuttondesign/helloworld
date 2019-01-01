27e0e08b6f5677d4ab9a0a4426a8b89c70caf1b1
chewdonkey <chewdonkey@gmail.com>
2019-01-01 13:34:50 +0000

    added c hello files

---
+++ hello.c
@@ -0,0 +1,22 @@
+//
+//PROGRAM TITLE: hello.c
+//PROGRAM DESCRIPTION: prints hello world & acts as c template
+//
+
+//include & define
+#include <stdio.h>
+#include "hellolib.h"
+
+//global variables
+
+//
+// FUNCTION DESCRIPTION: main
+//
+// INPUTS: none
+// OUTPUTS: 0 on success
+//
+int main()
+{
+    printf("\nhello world\n\n");
+    return 0;
+}
---
+++ hellolib.h
@@ -0,0 +1,17 @@
+//
+//LIBRARY TITLE: hellolib.h
+//LIBRARY DESCRIPTION: prints hello world & acts as c template
+//
+
+#ifndef hellolib_h
+#define hellolib_h
+
+//include & define
+
+//macros
+
+//global variables
+
+//function declarations
+
+#endif /* hellolib_h */
